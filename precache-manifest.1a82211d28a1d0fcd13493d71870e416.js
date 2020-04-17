afc7c8b99b007a9dd9450269627dfa95345e5752
Arun Prajapati <arunkp1122@gmail.com>
2020-04-17 15:53:31 +0545

    Updates

---
+++ precache-manifest.1a82211d28a1d0fcd13493d71870e416.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1aeaa40b43c52bba02d15a211e57158d",
+    "url": "/react-covid-tracker/index.html"
+  },
+  {
+    "revision": "694450f43eec0a70336d",
+    "url": "/react-covid-tracker/static/css/main.4137c00b.chunk.css"
+  },
+  {
+    "revision": "dd92e36948178374350d",
+    "url": "/react-covid-tracker/static/js/2.371ad945.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/react-covid-tracker/static/js/2.371ad945.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "694450f43eec0a70336d",
+    "url": "/react-covid-tracker/static/js/main.ccf732ad.chunk.js"
+  },
+  {
+    "revision": "fd35029ca0b8c5bd2d95",
+    "url": "/react-covid-tracker/static/js/runtime-main.35361316.js"
+  }
+]);
